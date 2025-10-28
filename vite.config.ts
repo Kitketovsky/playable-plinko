e47835441cbcd298a7be490641eb3756d5ebf86d
Kitketovsky <kitketovsky@gmail.com>
2025-10-28 12:12:36 +0400

    pixi.js project scaffold

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
+// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+    open: true,
+  },
   plugins: [
     viteSingleFile({
       removeViteModuleLoader: true,
